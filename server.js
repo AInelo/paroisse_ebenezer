750d7b33216e314b8468268ce59c8694ebbee85e
TOTON Lionel <totonlionel@gmail.com>
2024-01-12 18:58:27 +0100

    Pour l'automatisation de Mario

--- server.js
+++ server.js
@@ -1,34 +1,33 @@
-const http = require('http');
+const express = require('express');
+const app = express();
+const member = require('./routes/member');
+const connectDB = require('./db/connectDB');
+require('dotenv').config();
+const notFound = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
 
-// const express = require('express');
-// const app = express();
-// const member = require('./routes/member');
-// const connectDB = require('./db/connect');
-// require('dotenv').config();
-// const notFound = require('./middleware/not-found');
-// const errorHandlerMiddleware = require('./middleware/error-handler');
-
-// // middleware
-// app.use(express.static('./public'));
-// app.use(express.json());
+// middleware
+app.use(express.static('./public'));
+app.use(express.json());
 
 
 // // routes
-// app.use('/api/v1/tasks', tasks);
+// app.use('/api/v1/member', member);
 
-// app.use(notFound);
-// app.use(errorHandlerMiddleware);
-// const port = process.env.PORT || 5000;
+app.use(notFound);
+app.use(errorHandlerMiddleware);
 
-// const start = async () => {
-//   try {
-//     await connectDB(process.env.MONGO_URI);
-//     app.listen(port, () =>
-//       console.log(`Server is listening on port ${port}...`)
-//     );
-//   } catch (error) {
-//     console.log(error);
-//   }
-// };
 
-// start();
+const port = process.env.PORT || 5000;
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
