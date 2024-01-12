be33c1b1e288611bae5d4049d1c1c550ea8d0b47
TOTON Lionel <totonlionel@gmail.com>
2024-01-12 22:37:30 +0100

    Pour La paroisse Ebenezer

--- public/js/browseMember.js
+++ public/js/browseMember.js
@@ -1,50 +1,50 @@
-const tasksDOM = document.querySelector('.tasks')
-const loadingDOM = document.querySelector('.loading-text')
+const membersDOM = document.querySelector('.tasks')
+// const loadingDOM = document.querySelector('.loading-text')
 const formDOM = document.querySelector('.task-form')
 const taskInputDOM = document.querySelector('.task-input')
 const formAlertDOM = document.querySelector('.form-alert')
 // Load tasks from /api/tasks
-const showTasks = async () => {
+const showMembers = async () => {
   loadingDOM.style.visibility = 'visible'
   try {
     const {
-      data: { tasks },
-    } = await axios.get('/api/v1/tasks')
-    if (tasks.length < 1) {
-      tasksDOM.innerHTML = '<h5 class="empty-list">No tasks in your list</h5>'
-      loadingDOM.style.visibility = 'hidden'
+      data: { members },
+    } = await axios.get('/api/v1/member')
+    if (members.length < 1) {
+      membersDOM.innerHTML = '<h5 class="empty-list">No members in your list</h5>'
+    //   loadingDOM.style.visibility = 'hidden'
       return
     }
-    const allTasks = tasks
-      .map((task) => {
-        const { completed, _id: taskID, name } = task
-        return `<div class="single-task ${completed && 'task-completed'}">
+    const allMembers = members
+      .map((member) => {
+        const { completed, _id: memberID, name } = member
+        return `<div class="single-member ${completed && 'member-completed'}">
                   <h5>
                     <span>
                       <i class="far fa-check-circle">
                       </i>
                     </span>${name}
                   </h5>
-                  <div class="task-links">
+                  <div class="member-links">
 
                     <!-- edit link -->
-                    <a href="task.html?id=${taskID}"  class="edit-link">
+                    <a href="member.html?id=${memberID}"  class="edit-link">
                       <i class="fas fa-edit"></i>
                     </a>
                     <!-- delete btn -->
-                    <button type="button" class="delete-btn" data-id="${taskID}">
+                    <button type="button" class="delete-btn" data-id="${memberID}">
                       <i class="fas fa-trash"></i>
                     </button>
                   </div>
                 </div>`
       })
       .join('')
-    tasksDOM.innerHTML = allTasks
+    tasksDOM.innerHTML = allMembers
   } catch (error) {
     tasksDOM.innerHTML =
       '<h5 class="empty-list">There was an error, please try later....</h5>'
   }
-  loadingDOM.style.visibility = 'hidden'
+//   loadingDOM.style.visibility = 'hidden'
 }
 
 showTasks()
@@ -54,7 +54,7 @@ showTasks()
 tasksDOM.addEventListener('click', async (e) => {
   const el = e.target
   if (el.parentElement.classList.contains('delete-btn')) {
-    loadingDOM.style.visibility = 'visible'
+    // loadingDOM.style.visibility = 'visible'
     const id = el.parentElement.dataset.id
     try {
       await axios.delete(`/api/v1/tasks/${id}`)
@@ -63,28 +63,5 @@ tasksDOM.addEventListener('click', async (e) => {
       console.log(error)
     }
   }
-  loadingDOM.style.visibility = 'hidden'
-})
-
-// form
-// To create a TASK
-formDOM.addEventListener('submit', async (e) => {
-  e.preventDefault()
-  const name = taskInputDOM.value
-
-  try {
-    await axios.post('/api/v1/tasks', { name })
-    showTasks()
-    taskInputDOM.value = ''
-    formAlertDOM.style.display = 'block'
-    formAlertDOM.textContent = `success, task added`
-    formAlertDOM.classList.add('text-success')
-  } catch (error) {
-    formAlertDOM.style.display = 'block'
-    formAlertDOM.innerHTML = `error, please try again`
-  }
-  setTimeout(() => {
-    formAlertDOM.style.display = 'none'
-    formAlertDOM.classList.remove('text-success')
-  }, 3000)
+//   loadingDOM.style.visibility = 'hidden'
 })
