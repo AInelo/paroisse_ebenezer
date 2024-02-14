eb33893e966e5741b71203bee585863108a27367
TOTON Lionel <totonlionel@gmail.com>
2024-02-14 18:23:47 +0100

    Pour La paroisse Ebenezer

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
+
+const os = require('os');
+const path = require('path');
 const member = require('./routes/member');
 const connectDB = require('./db/connectDB');
 require('dotenv').config();
@@ -18,12 +21,32 @@ app.use(notFound);
 app.use(errorHandlerMiddleware);
 
 
+
+ // Obtenez l'adresse IP de la première interface réseau non interne
+ const networkInterfaces = os.networkInterfaces();
+ // console.log(networkInterfaces);
+ const ipAddress = Object.values(networkInterfaces)
+   .flat()
+   .filter((iface) => iface.family === 'IPv4' && !iface.internal)[0].address;
+
+ // console.log(ipAddress);
+
+//  const Port = process.env.PORT || 3000;
+
+//  app.listen(Port, ipAddress, () => {
+//    console.log(`Le serveur écoute sur ${ipAddress}:${Port}.....`);
+//  });
+
+
+
+
+
 const port = process.env.PORT || 5000;
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URI);
-    app.listen(port, () =>
-      console.log(`Server is listening on port ${port}...`)
+    app.listen(port,ipAddress, () =>
+      console.log(`Server is listening on port ${ipAddress}:${port}...`)
     );
   } catch (error) {
     console.log(error);
