84ca4d74385007c09f7754bfb51afcd0968524d7
TOTON Lionel <totonlionel@gmail.com>
2024-02-17 17:17:30 +0100

    Pour La paroisse Ebenezer

--- public/js/updateMember.js
+++ public/js/updateMember.js
@@ -24,11 +24,12 @@ const showMembers = async () => {
         <img class="mx-auto mb-4 w-36 h-36 rounded-full" src="${url}" alt="Photo des Fidèles">
         <h3 class="mb-1 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
         <div class="">
-        <input type="text" name="email" id="email" class="col-span-1 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500  p-2.5" placeholder="${name}" required>
-        <input type="text" name="email" id="email" class="col-span-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500  p-2.5" placeholder="${lastname}" required>
+          <input type="text" name="email" id="email" class="py-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500  p-2.5" placeholder="${name}" required>
+          <input type="text" name="email" id="email" class="py-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500  p-2.5" placeholder="${lastname}" required>
         </div>
-            <a href="">${name} ${lastname}</a>
-        </h3>
+
+        <button type="submit" class="mt-4 w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Enregistrer</button>
+
         <p>Fidèle</p>
             <ul class="flex justify-center mt-4 space-x-4">
                 <li>
