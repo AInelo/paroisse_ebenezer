3a9ccc5eb7b79f165dc32216576ca571460c55ad
TOTON Lionel <totonlionel@gmail.com>
2024-02-18 10:05:09 +0100

    Pour La paroisse Ebenezer

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,7 +3,6 @@ const asyncWrapper = require('../middleware/async');
 const { createCustomError } = require ('../errors/custom-error')
 
 
-
 const signUp = asyncWrapper(async (req, res) => {
     const { username, password } = req.body;
 
@@ -16,3 +15,24 @@ const signUp = asyncWrapper(async (req, res) => {
     console.log('Record Inserted Successfully');
     res.redirect('auth.html');
 });
+
+
+const signIn = asyncWrapper(async (req, res) => {
+    const { email, password } = req.body;
+
+    // Rechercher l'utilisateur par email
+    const user = await User.findOne({ email });
+
+    if (!user || user.password !== password) {
+        return res.status(401).json({ message: 'Invalid credentials' });
+        // throw createCustomError('Invalid credentials', 401);
+    }
+
+    // Authentification réussie
+    res.redirect('signin_success.html');
+});
+
+module.exports = {
+    signUp,
+    signIn
+}
