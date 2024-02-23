0e0edd0b4d7688c4b1ff54e3f36f54cc2ae71b93
TOTON Lionel <totonlionel@gmail.com>
2024-02-23 10:40:44 +0100

    Pour La paroisse Ebenezer

--- controllers/memberController.js
+++ controllers/memberController.js
@@ -46,6 +46,32 @@ const updateMember = asyncWrapper(async (req, res, next) => {
   res.status(200).json({ member })
 })
 
+
+
+
+const postImage = asyncWrapper(async (req, res) => {
+  try {
+    if (!req.file) {
+      return res.status(500).json({ error: "No file found" });
+    }
+    
+    const imageFile = Image({
+      filename: req.file.filename,
+      filepath: req.file.path,
+    });
+
+    const savedImage = await imageFile.save();
+
+    res.status(200).json(savedImage);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+
+
+
+
 module.exports = {
   getAllMembers,
   createMember,
