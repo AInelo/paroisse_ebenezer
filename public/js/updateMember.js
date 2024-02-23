6463576571a958f34bba7f06c203dbefaf561e31
TOTON Lionel <totonlionel@gmail.com>
2024-02-23 19:07:14 +0100

    Pour La paroisse Ebenezer

--- public/js/updateMember.js
+++ public/js/updateMember.js
@@ -136,6 +136,8 @@ const selectImage = document.querySelector('.select-image');
 const inputFile = document.querySelector('#file');
 const imgArea = document.querySelector('.img-area');
 
+
+
 selectImage.addEventListener('click', function () {
 	inputFile.click();
 })
